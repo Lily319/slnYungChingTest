657984b91688a47445caa1dbf85d18c554ee28cb
Lily319 <leftbehindgd2009@gmail.com>
2022-08-18 11:04:23 +0800

    加入專案檔案。

---
+++ prjYungChingTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using prjYungChingTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjYungChingTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private NorthwindContext db;
+
+        public HomeController(ILogger<HomeController> logger, NorthwindContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public IActionResult Employees()
+        {
+            var e = db.Employees;
+            return View(e);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ prjYungChingTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prjYungChingTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
